a50315b7236303105b490cb3cac5fb32578d8303
Kacper Doga <doga.kacper@gmail.com>
2025-07-02 23:46:10 +0200

    test(recording): starting/stoping recording, modifying flag

---
+++ sentinel-agent/tests/RecordingManager_test.cpp
@@ -0,0 +1,41 @@
+//
+// Created by varev on 7/1/25.
+//
+#include "recording/RecordingManager.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(RecordingManagerTest, StartRecordingSetsFlag) {
+    RecordingManager recorder;
+    EXPECT_FALSE(recorder.isRecording());
+
+    recorder.startRecording("dummy.avi");
+    EXPECT_TRUE(recorder.isRecording());
+}
+
+TEST(RecordingManagerTest, StopsRecordingClearsFlag) {
+    RecordingManager recorder;
+    recorder.startRecording("dummy.avi");
+    recorder.stopRecording();
+
+    EXPECT_FALSE(recorder.isRecording());
+}
+
+TEST(RecordingManagerTest, StartsRecordingOnMotion) {
+    RecordingManager recorder;
+    EXPECT_FALSE(recorder.isRecording());
+
+    recorder.onMotionDetected();
+    EXPECT_TRUE(recorder.isRecording());
+}
+
+TEST(RecordingManagerTest, StopsRecordingAfterStableFramesThreshold) {
+    RecordingManager recorder;
+    recorder.onMotionDetected();
+    EXPECT_TRUE(recorder.isRecording());
+
+    for (uint32_t i = 0; i < recorder.getStableFramesThreshold(); ++i)
+        recorder.onNoMotion();
+
+    EXPECT_FALSE(recorder.isRecording());
+}
